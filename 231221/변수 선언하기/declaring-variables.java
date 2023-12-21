27a6282b510584ea4bbfd474250c62f46dd85350
yeoon <52392720+yeooniyeoon@users.noreply.github.com>
2023-12-21 14:10:40 +0900

    Title: 변수 선언하기, Time: 84ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231221/변수 선언하기/declaring-variables.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        char c = 'c';
+        System.out.println(a);
+        System.out.println(c);
+    }
+}
